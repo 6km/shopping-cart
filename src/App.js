27f74049d0c4accec1a89c577e5bcce2d5ed8bc6
= <=>
2021-02-08 22:23:11 +0200

    |

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [
+        { id:0, name: 'Product 1', quantity: 4 },
+        { id:1, name: 'Product 2', quantity: 7 }
+      ]
+    }
+  }
 
-export default App;
+  add = (id) => {
+    this.state.items[id].quantity += 1
+    this.setState(this.state)
+  }
+  remove = (id) => {
+    if (this.state.items[id].quantity > 0) {
+      this.state.items[id].quantity -= 1
+    }
+    this.setState(this.state)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="Container">
+          <div id="ContainerList">
+            {
+              this.state.items.map(item => 
+                <div className="ContainerListItem">
+                  {item.name}
+                  <button onClick={() => this.add(item.id)}>+</button>
+                  <button style={item.quantity <= 0 ? {background:'orange'}: {}} disabled>{item.quantity>0 ? item.quantity : 'Zero'}</button>
+                  <button onClick={() => this.remove(item.id)}>-</button>
+                </div>
+              )
+            }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
